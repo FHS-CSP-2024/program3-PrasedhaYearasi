1e1612cb2ac068d5e3cfce5f9044a7e312b07313
PrasedhaYearasi <prasedhayearasi@gmail.com>
2024-09-09 15:18:41 +0000

    I finished program 3

--- Program3.py
+++ Program3.py
@@ -22,12 +22,12 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+#print("my name is ", name, " , I am ", age, "years old")
+#print("my skills are")
+#print("- ", skill1, level1, )
+#print("- ", skill2,  level2, )
+#print("- ", skill3, level3, )
+#print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
 
 
 
@@ -48,6 +48,12 @@ print("I am looking for a job with a salary of", lower, "-", upper, "dollars per
 #
 # - The program should work correctly even if the values of the variables are changed.
 
-x = input("X val: ")
-y = input("Y val: ")
+x = input("X val")
+y = input("Y val:")
+print(int(x) + int(y))
+print(int(x) - int(y)) 
+print(int(x) * int(y))
+print(int(x) / int(y))
+
+
 
